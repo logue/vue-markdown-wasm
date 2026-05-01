7b52ce6bd9e125973d067861500fece1a841365a
logue <logue@hotmail.co.jp>
2026-05-01 09:43:08 +0000

    Deploying to gh-pages from @ logue/vue-markdown-wasm@01b2898845ba194a78acfec08a74eb185bfca698 🚀

---
+++ assets/__vite-browser-external-Bxi5nLRO.js
@@ -0,0 +1,12 @@
+/**
+ * vue-markdown-wasm
+ *
+ * @description markdown-wasm for vue 2 & 3
+ * @author Logue <logue@hotmail.co.jp>
+ * @copyright 2022-2026 By Masashi Yoshikawa All rights reserved.
+ * @license MIT
+ * @version 1.1.0
+ * @see {@link https://github.com/logue/vue-markdown-wasm}
+ */
+
+import{t as e}from"./index-COEgwKfW.js";var t=e(((e,t)=>{t.exports={}}));export default t();
